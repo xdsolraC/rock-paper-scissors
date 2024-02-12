290aea17e23f710a84ba0d0b7badad0d68b08013
xdsolraC <xds0lr4c@gmail.com>
2024-02-12 16:03:11 -0400

    JS: rework getComputerChoice function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"]
-    return choices[Math.round(Math.random() * 2)];
+    const choices = ["Rock", "Paper", "Scissors"]
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function getResults(playerSelection, computerSelection = getComputerChoice()) {
