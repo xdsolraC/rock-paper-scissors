7ecd9169e47d6100d8aa77a13abcaf00412aa7b6
xdsolraC <xds0lr4c@gmail.com>
2024-02-12 16:28:15 -0400

    JS: rework playRound function

--- index.js
+++ index.js
@@ -63,53 +63,14 @@ function displayResults(results) {
     }
 }
 
-function playRound(playerSelection, computerSelection = getComputerChoice()) {
-    let userChoice = playerSelection.toLowerCase();
-    let computerChoice = computerSelection.toLowerCase();
-
-    if (userChoice == "rock" && computerChoice == "scissors") {
-        return {
-            winner: "player",
-            message: "You Win! Rock beats Scissors"
-        }
+function playRound(playerSelection) {
+    if (roundsPlayed >= 9 || playerWins >= 5 || computerWins >= 5) {
+        return;
     }
 
-    else if (userChoice == "rock" && computerChoice == "paper") {
-        return {
-            winner: "computer",
-            message: "You Lose! Paper beats Rock"
-        }
-    }
+    const results = getResults(playerSelection);
 
-    else if (userChoice == "paper" && computerChoice == "rock") {
-        return {
-            winner: "player",
-            message: "You Win! Paper beats Rock"
-        }
-    }
-
-    else if (userChoice == "paper" && computerChoice == "scissors") {
-        return {
-            winner: "computer",
-            message: "You Lose! Scissors beats Paper"
-        }
-    }
-
-    else if (userChoice == "scissors" && computerChoice == "paper") {
-        return {
-            winner: "player",
-            message: "You Win! Scissors beats Paper"
-        }
-    }
-
-    else if (userChoice == "scissors" && computerChoice == "rock") {
-        return {
-            winner: "computer",
-            message: "You Lose! Rock beats Scissors"
-        }
-    }
-
-    return playRound(playerSelection)
+    displayResults(results);
 }
 
 // Global variables
